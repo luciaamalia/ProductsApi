2e852b744706acbfb2c43094706733da5fa675a0
luciaamalia <lucia.nogueira@gmail.com>
2024-10-31 17:55:52 -0300

    teste

---
+++ UsersApi/src/main/java/com/users/Users/models/UserModel.java
@@ -0,0 +1,35 @@
+package com.users.Users.models;
+
+import com.users.Users.enums.TypeUser;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "users")
+@Table(name = "users")
+public class UserModel {
+
+    @Id
+    @Column(name = "id_user")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID idUser;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String email;
+
+    @Column(name = "type_user")
+    @Enumerated(EnumType.STRING)
+    private TypeUser typeUser;
+
+}
